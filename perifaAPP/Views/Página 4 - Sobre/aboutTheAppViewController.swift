8e77e8e102e523185453735dea90733d522a1b8b
Gabriel Batista <gabriel.devproject@gmail.com>
2022-01-12 19:12:52 -0300

    update aboutViewController

--- perifaAPP/Views/Página 4 - Sobre/aboutTheAppViewController.swift
+++ perifaAPP/Views/Página 4 - Sobre/aboutTheAppViewController.swift
@@ -3,65 +3,56 @@
 //  perifaAPP
 //
 //  Created by Thallis Sousa on 19/07/21.
-//
+
 
 import UIKit
 
-class aboutTheAppViewController: UIViewController {
+class aboutTheAppViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var indicacaoDeLocal: UIButton!
     @IBOutlet weak var sobreNos: UILabel!
     @IBOutlet weak var juntosSomosNos: UILabel!
     @IBOutlet weak var tJuntosSomosNos: UITextView!
     
     public let adaptativeColor = UIColor(named: "aColor")
     
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let cellTitles = ["Sobre o SPerifa",
+                              "Avaliar o aplicativo",
+                              "Compartilhar aplicativo",
+                              "Enviar sugestão de local",
+                              "Reportar erro"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray6
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.frame = CGRect(x: 10, y: 30, width: (view.frame.size.width) - 20, height: 230)
+        tableView.clipsToBounds = true
+        tableView.backgroundColor = .white
+        self.tableView.layer.cornerRadius = 14.0
+        
+        var frame = CGRect.zero
+        frame.size.height = .leastNormalMagnitude
+        tableView.tableHeaderView = UIView(frame: frame)
+        
+        view.addSubview(tableView)
+
+        
         configNavBar()
-        setConstraints()
-        configButton()
-        configTexto()
-        
     }
     
-    func configButton() {
-        //MARK: configuração do botão de Indicação de locais
-        indicacaoDeLocal.layer.cornerRadius = 8
-        indicacaoDeLocal.backgroundColor = UIColor(named: "OrangeApp")
-        indicacaoDeLocal.setTitle("Indicação de locais", for: .normal)
-        indicacaoDeLocal.tintColor = .white
-        self.view.addSubview(indicacaoDeLocal)
-        
-    }
-    //MARK: atributos dos textos na view de "Sobre", com as cores selecionadas.
-    func configTexto() {
-        sobreNos.textColor = UIColor(named: "adaptativeColor")
-        self.view.addSubview(sobreNos)
-        
-        juntosSomosNos.text = "Juntos, somos nós"
-        juntosSomosNos.textColor = .white
-        self.view.addSubview(juntosSomosNos)
-    }
+
     
-    
-    func setConstraints () {
-        //MARK: constraints do texto do botão
-        indicacaoDeLocal.translatesAutoresizingMaskIntoConstraints = false
-        
-        let indicacaoDeLocal: [NSLayoutConstraint] = [
-            indicacaoDeLocal.heightAnchor.constraint(equalToConstant: 45),
-            indicacaoDeLocal.widthAnchor.constraint(equalToConstant: 250),
-            indicacaoDeLocal.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            indicacaoDeLocal.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
-        ]
-        
-        NSLayoutConstraint.activate(indicacaoDeLocal)
-        
-    }
     
     func configNavBar() {
         //MARK: configurações da navigation e da NavBar
@@ -70,10 +61,28 @@ class aboutTheAppViewController: UIViewController {
         let navBarAppearance = UINavigationBarAppearance()
         navBarAppearance.configureWithOpaqueBackground()
         navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-        navBarAppearance.backgroundColor = .systemOrange
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor(named: "OrangeApp") as Any]
+        navBarAppearance.backgroundColor = .white
         navigationController?.navigationBar.standardAppearance = navBarAppearance
         navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = cellTitles[indexPath.row]
+        content.textProperties.color = UIColor(named: "OrangeApp")!
+        
+        cell.contentConfiguration = content
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
     }
     
 }
